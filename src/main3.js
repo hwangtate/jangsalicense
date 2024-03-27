803a4518c204b69f0064b5f2294ee22c50be7d5b
hwang tate <ghkdxodud10253504@gmail.com>
2024-03-27 16:55:39 +0900

    web v2

--- src/main3.js
+++ src/main3.js
@@ -7,7 +7,7 @@ function Main3() {
       <div className='main3-background'>
         <img src='img/main3/text1.png' className='main3-img1' alt='이미지'/>
         <img src='img/main3/text2.png' className='main3-img2' alt='이미지'/>
-        <img src='img/main3/text3.png' className='main3-img3'  alt='이미지'/>
+        <img src='img/main3/text4.png' className='main3-img3'  alt='이미지'/>
       </div>
     </>
   );
